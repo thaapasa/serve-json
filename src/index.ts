d98b91d70d577e2555996c8382f014e201044b32
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-04-20 15:37:30 +0300

    Implement basic file serving and add example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 const server = require('server');
 const debug = require('debug')('serve-json');
 
-const { get, post } = server.router;
+const { json } = server.reply;
+const dir = process.argv[2] || process.env.PWD;
 
-debug('Starting server...');
+debug('Serving files under', dir, process.argv);
 
-server({ port: 8080, security: false }, (ctx: any) => 'Hello any!');
+server({ port: 8080, security: false }, (ctx: any) => renderFile(ctx.path, ctx.method));
+
+function renderFile(path: string, method: string): Promise<any> {
+  try {
+    const filePath = `${dir}${path}.${method}.json`;
+    const data = require(filePath);
+    debug(method, path, '200 OK');
+    return json(data);
+  } catch (e) {
+    debug(method, path, '404 Not found');
+    return server.reply.status(404).json({ type: 'error', message: 'File not found' });
+  }
+}
